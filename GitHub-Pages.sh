01317ddbcffb97e16f1fab2fba9427a9ef0153b8
xiaofeng.zhu <34299738+zxffffffff@users.noreply.github.com>
2023-11-20 22:42:37 +0800

    发布docs

---
+++ GitHub-Pages.sh
@@ -0,0 +1,7 @@
+mkdir -p docs
+mkdir -p docs/build
+
+cp index.html            docs/index.html
+cp index.js              docs/index.js
+cp build/stockchart.wasm docs/build/stockchart.wasm
+cp build/stockchart.js   docs/build/stockchart.js
